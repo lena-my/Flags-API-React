d04bc0d9a97804ac05b179bb768f188556f759cb
lena-my <helenamiura@gmail.com>
2024-02-09 11:05:18 +0100

    add style nav.isAtive

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const navigation = () => {
-    return (
-        <div>
-            <div className="navigation">
-                <ul>
-                    <NavLink to="/">
-                        <li>accueil</li>
-                    </NavLink>
-                    <NavLink to="/about">
-                        <li>à propos</li>
-                    </NavLink>
-                </ul>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="navigation">
+        <ul>
+          <NavLink
+            to="/"
+            className={(nav) => (nav.isActive ? "nav-active" : "")}
+          >
+            <li>accueil</li>
+          </NavLink>
+          <NavLink
+            to="/about"
+            className={(nav) => (nav.isActive ? "nav-active" : "")}
+          >
+            <li>à propos</li>
+          </NavLink>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default navigation;
