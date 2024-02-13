8f0982b596216f80f52f705666313895d26cd04c
lena-my <helenamiura@gmail.com>
2024-02-13 15:13:23 +0100

    add useEffect/ useState

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const Countries = () => {
+    const [data, setData] = useState([])
+    const [hello, setHello] = useState('Hello les amis')
 
-    
+    //Le useEffect se joue lorsque le composant est monté
+    useEffect(() => { 
+        axios
+        .get("https://restcountries.com/v3.1/all")
+        .then((res) => setData(res.data))
+    }, [])
 
     return (
         <div className='countries'>
             <h1>COUNTRIES</h1>
+            <ul>
+                {
+                    hello
+                }
+            </ul>
         </div>
     );
 };
